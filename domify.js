84b44e6a20d76462f3d5f1a473664d27c913aed5
nickster716 <nikhil716716@gmail.com>
2024-12-14 19:14:06 +0530

    Add files via upload

---
+++ domify.js
@@ -0,0 +1,69 @@
+// let section = document.createElement('section');
+// section.classList.add("row");
+
+// // first box of the row
+// let div1 = document.createElement("div");
+// div1.classList.add("article");
+
+// let hfirst = document.createElement("h2");
+// hfirst.textContent= "Article 1";
+
+// img1= document.createElement("img");
+// img1.src="https://picsum.photos/1200/1200?random=1";
+// img1.classList.add("article-image");
+// img1.alt="a picture";
+
+// let para1= document.createElement("p");
+// para1.textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit illo";
+
+// div1.appendChild(hfirst);
+// div1.appendChild(img1);
+// div1.appendChild(para1);
+// // first box of the row
+
+// // second box of the row
+// let div2 = document.createElement("div");
+// div2.classList.add("article");
+
+// let hsecond = document.createElement("h2");
+// hsecond.textContent= "Article 2";
+
+// img2= document.createElement("img");
+// img2.src="https://picsum.photos/1200/1200?random=2";
+// img2.classList.add("article-image");
+// img2.alt="a picture";
+
+// let para2= document.createElement("p");
+// para2.textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit illo";
+
+// div2.appendChild(hsecond);
+// div2.appendChild(img2);
+// div2.appendChild(para2);
+// // second box of the row
+
+// // third box of the row
+// let div3 = document.createElement("div");
+// div3.classList.add("article");
+
+// let hthird = document.createElement("h2");
+// hthird.textContent= "Article 3";
+
+// img3= document.createElement("img");
+// img3.src="https://picsum.photos/1200/1200?random=3";
+// img3.classList.add("article-image");
+// img3.alt="a picture";
+
+// let para3= document.createElement("p");
+// para3.textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit illo";
+
+// div3.appendChild(hthird);
+// div3.appendChild(img3);
+// div3.appendChild(para3);
+// // third box of the row
+
+// section.appendChild(div1);
+// section.appendChild(div2);
+// section.appendChild(div3);
+
+// let mainpicend = document.querySelector(".hero");
+// mainpicend.insertAdjacentElement("afterend",section);
